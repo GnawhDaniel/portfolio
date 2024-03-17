26fbfb36a76add9f5cda5f81b2431ae9d2873e9a
Daniel Hwang <danielhwangdeveloper@gmail.com>
2024-03-16 21:38:09 -0500

    init.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'bash-green': '#00ff00',
+        'consolas': "#5271ff"
       },
+      fontFamily: {
+        'mono': ['Ubuntu Mono', 'monospace'],
+      },
+      fontSize: {
+        'ascii-sm': '0.4rem'
+      }
     },
   },
   plugins: [],
